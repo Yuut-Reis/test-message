f0f8aa90457b0f3bbe8e882f0594d81311c57460
Yuut <ingride.reis.b@gmail.com>
2022-10-09 11:45:09 -0300

    test(test): :test_tube: Testing if the Print message function has the expected return

--- src/test-message.Test/TestPrint.cs
+++ src/test-message.Test/TestPrint.cs
@@ -7,20 +7,23 @@ namespace test_message.Test;
 
 public class TestPrint
 {
-    [Theory]
-    [InlineData("", "")]
+    [Theory(DisplayName = "Testando se a função Printmsg imprime a menssagem corretamente no Terminal.")]
+    [InlineData("Estamos sempre em busca da nossa melhor versão!", "Estamos sempre em busca da nossa melhor versão!")]
+    [InlineData("Aplicando testes com xUnit", "Aplicando testes com xUnit")]
+    [InlineData("C# é uma linguagem incrivel", "C# é uma linguagem incrivel")]
+    [InlineData("Meu console está funcionando corretamente", "Meu console está funcionando corretamente")]
+    [InlineData("Não fique triste eu te consolo", "Não fique triste eu te consolo")]
     public void TestPrintMsgSucess(string Send, string Expected)
     {
-        throw new NotImplementedException();
-        /*using (var NewOutput = new StringWriter())
+          using (var NewOutput = new StringWriter())
         {
             Console.SetOut(NewOutput);
 
-            Message.PrintMsg("");
+            Message.PrintMsg(Send);
 
             string result = NewOutput.ToString().Trim();
 
-            result.Should();      
-        }*/
+            result.Should().Be(Expected);      
+        }
     }
 }
